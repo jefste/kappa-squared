44b17f5d8251912c90840eca9147b2358785f79e
Jeff Stevens <jefste@gmail.com>
2014-11-24 11:33:41 -0500

    add function to grab XYZ coordinates and grab tryptophan coordinates of interest for tryptophan 7

--- kappa-squared.py
+++ kappa-squared.py
@@ -23,3 +23,20 @@ data = []
 for row in datafile:
     data.append(row.strip().split())
 
+
+# grabs XYZ coordinates from a given row and returns them as a list of 3 dimensions
+def getXYZfromRow(row):
+    return [float(row[6]),float(row[7]),float(row[8])]
+
+
+#this grabs the tryptophan coordinates, only for the specific case. 
+#should change this so that the range is more flexible, and also so that it prompts for which tryptophan is of interest
+for row in data[372:386]:
+    if row[2]=='CD1':
+        trpCD1=getXYZfromRow(row)
+    if row[2]=='CD2':
+        trpCD2=getXYZfromRow(row)
+    if row[2]=='CE2':
+        trpCE2=getXYZfromRow(row)
+    if row[2]=='NE1':
+        trpNE1=getXYZfromRow(row)
