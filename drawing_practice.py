67dfe2ebd4dbf6e589312871a5ae4e9fe45c57ec
Jeff Stevens <jefste@gmail.com>
2014-11-29 00:19:28 -0500

    Add functionality to merge coordinates for plotting

--- drawing_practice.py
+++ drawing_practice.py
@@ -105,13 +105,28 @@ grabPDB()
 readFromDatafile('1MBD')
 getTrpCoordfromPDB(7)
 getHemeCoordfromPDB()
-print atomXYZ
+#print atomXYZ
+
 
 
 fig = matplotlib.pyplot.figure()
 ax  = fig.add_subplot(111, projection = '3d')
-print atomXYZ['CD1']
-ax.plot(atomXYZ['CD1'],atomXYZ['CHC'],color = 'r')
+
+#plot reads coordinates as [x1,x2,x3...],[y...][z...] so need to split up the function
+# there is probably a more elegant way to write this, but this works for now
+# make the return work for x, y, z not just one coordinate. I tried 3 but it didnt work
+def mergeCoordinatesForPlotting(listOfkeys,index):
+	x=[]
+	for key in listOfkeys:
+		x.append(atomXYZ[key][index])
+	return x
+
+#simplify for readibility
+mCFP=mergeCoordinatesForPlotting
+#plots sets of coordinates
+#
+ax.plot(mCFP(trpList,0),mCFP(trpList,1),mCFP(trpList,2),color = 'r')
+ax.plot(mCFP(hemeList,0),mCFP(hemeList,1),mCFP(hemeList,2),color = 'b')
 
 
 matplotlib.pyplot.show()
