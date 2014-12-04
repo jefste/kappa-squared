34cfc0483e3f0c560b317c5181ad77f780155981
Jeff Stevens <jefste@gmail.com>
2014-12-03 22:56:08 -0500

    Add comments to parseCon... function

--- drawing_practice.py
+++ drawing_practice.py
@@ -4,6 +4,7 @@ from mpl_toolkits.mplot3d import Axes3D
 #by adding this line, i can get rid of all my other code that was copied from the other python script
 from kappaSquared import *
              
+
 grabPDB()
 readFromDatafile('1MBD')
 getTrpCoordfromPDB(7)
@@ -34,7 +35,7 @@ addListtoPlot(['NE1','trpOrigin'],ax,'b')
 addListtoPlot(['FE','trpOrigin'],ax,'g')
 
 addListtoPlot(['CHC','FE','CHD'],ax,'r')
-#ax.plot(mCFP(hemeList,0),mCFP(hemeList,1),mCFP(hemeList,2),color = 'b')
+ax.plot(mCFP(hemeList,0),mCFP(hemeList,1),mCFP(hemeList,2),color = 'b')
 
 
 #shows plot
@@ -44,10 +45,13 @@ addListtoPlot(['CHC','FE','CHD'],ax,'r')
 
 
 def parseConnectList(listtoparse,startCoordinate):
+    #change this function to only add the entry before and after the atom of interest. 
+    #don't need every single connection, only the ones that are connected to the atom of interest.    
     mylist=[]    
     print startCoordinate
     for row in listtoparse:
-        for item in row:        
+        for item in row:
+            #adds line, but omits 'Conect' command.        
             if str(item) != 'CONECT':
                 mylist.append(item)  
         #add newline as place holder for now. need to decide best way forward on combining the atom numbers.q
